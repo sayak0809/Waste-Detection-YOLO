e48dcf1e5989b8fb264ffd151fab3740fa67b80f
Sayak Mallick <mallicksayak@gmail.com>
2024-09-27 15:59:00 +0200

    added requirements

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages,setup
+
+setup (
+    name = "wasteDetection",
+    version = "0.0.0",
+    author = "Sayak Mallick",
+    author_email = "mallicksayak@gmail.com",
+    packages = find_packages(),
+    install_requires = []
+)
